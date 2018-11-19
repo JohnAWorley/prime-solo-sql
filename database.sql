091869131ab30c97a8a7aa7e5345d699e9c502c5
John Worley <john.aver.worley@gmail.com>
2018-11-19 17:20:51 -0600

    done

---
+++ database.sql
@@ -0,0 +1,24 @@
+-- 1. SELECT "username" FROM "accounts"
+-- WHERE "city" LIKE '%chicago';
+
+-- 2. SELECT "username" FROM "accounts"
+-- WHERE "username" LIKE '%a'; 
+
+-- 3. UPDATE "accounts" SET "account_balance" = 10
+-- WHERE "account_balance"  = 0 and "transactions_attempted" = 0;
+
+-- 4. SELECT *  FROM "accounts"
+-- WHERE "transactions_attempted" >= 9;
+
+-- 5. SELECT * FROM "accounts"
+-- ORDER BY "account_balance" DESC
+-- LIMIT 3;
+
+-- 6. SELECT "username"  FROM "accounts"
+-- ORDER BY "account_balance" ASC
+-- LIMIT 3;
+
+-- 7. SELECT "username"  FROM "accounts"
+-- WHERE "account_balence" > 100;
+
+--8 UPDATE "accounts" SET "username" = 'john' etc etc
